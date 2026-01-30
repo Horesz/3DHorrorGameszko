a640d915eade82293ef891af615ba4bcef601c7a
BMartin <bobak.martin03@gmail.com>
2026-01-30 09:14:41 +0100

    Start kidolgozása

--- Assets/Scripts/Main_manu_script.cs
+++ Assets/Scripts/Main_manu_script.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Main_manu_script
+public class Main_manu_script : MonoBehaviour
 {
-    
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Hospital_main");
+    }
 }
